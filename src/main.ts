f304cfec7ede221892418f549b80505fea82e18a
Leonam Quintão <leonamquintao@gmail.com>
2018-10-17 08:49:07 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+declare const module: any;
+const port: number = parseInt(process.env.PORT) || 9090;
+const host: string = process.env.HOST;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port);
+  Logger.log(`Server running in ${host}:${port}`);
+
+  if(module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+}
+bootstrap();
