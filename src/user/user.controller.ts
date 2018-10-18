59b4e69b77d4c50c4ac544cc7bd5fb4b037d73c3
Leonam Quintão <leonamquintao@gmail.com>
2018-10-18 14:03:21 -0300

    User's crud finished

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,6 @@
-import { Get, Post, Put, Delete, Controller } from '@nestjs/common';
+import { Get, Post, Put, Delete, Param, Body, Controller } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserDTO } from './user.dto';
 
 @Controller('users')
 export class UserController {
@@ -7,27 +8,27 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Get()
-  public getUsers(): any {
+  public getUsers() {
     return this.userService.getUsers();
   }
 
   @Get(':id')
-  public getUser(id): any {
+  public getUser(@Param('id') id: string) {
     return this.userService.getUser(id);
   }
 
   @Post()
-  public createUser(): any {
-
+  public createUser(@Body() data: Partial<UserDTO>) {
+    return this.userService.createUser(data);
   }
 
   @Put(':id')
-  public updateUser() {
-
+  public updateUser(@Param('id') id: string, @Body() data: Partial<UserDTO>) {
+    return this.userService.updateUser(id, data);
   }
 
   @Delete(':id')
-  public deleteUserById() {
-
+  public deleteUserById(@Param('id') id: string) {
+    return this.userService.deleteUserById(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { UserDTO } from './user.dto';
 import { DatabaseService } from '../shared/database/database.service';
+
+const ObjectId = require('mongodb').ObjectId
 
 @Injectable()
 export class UserService {
@@ -14,20 +17,26 @@ export class UserService {
 
   public async getUser(id: string): Promise <any> {
     let db = await this.db.connect();
-    let result = await db.collection("users").find({"_id": id}).toArray();
+    let result = await db.collection("users").find({"_id": ObjectId(id)}).toArray();
     return result;
   }
 
-  public async createUser(data: any): Promise <any> {
-
+  public async createUser(data: Partial<UserDTO>): Promise <any> {
+    let db = await this.db.connect();
+    let insert = await db.collection("users").insert(data);
+    return insert
   }
 
-  public async updateUser(id: string, data: any): Promise <any> {
-
+  public async updateUser(id: string, data: Partial<UserDTO>): Promise <any> {
+    let db = await this.db.connect();
+    let update = await db.collection("users").update({ "_id": ObjectId(id) }, { $set: data  });
+    return update
   }
 
   public async deleteUserById(id: string): Promise <any> {
-
+    let db = await this.db.connect();
+    let rm = await db.collection("users").deleteOne({"_id": ObjectId(id)});
+    return rm
   }
 
 }
