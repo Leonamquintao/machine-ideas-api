ed3670b7c30a6611110ea9aafaa22c292c05b830
Leonam Quintão <leonamquintao@gmail.com>
2018-10-19 09:36:04 -0300

    idea entety created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SharedModule } from './shared/shared.module';
-import { UserModule } from './user/user.module';
+
 import { HttpErrorFilter } from './shared/http/httpError.filter';
 import { APP_FILTER } from '@nestjs/core';
 
+import { SharedModule } from './shared/shared.module';
+import { UserModule } from './user/user.module';
+import { IdeaModule } from './idea/idea.module';
+
 @Module({
-  imports: [SharedModule, UserModule],
+  imports: [SharedModule, UserModule, IdeaModule],
   controllers: [AppController],
   providers: [AppService, { provide: APP_FILTER, useClass: HttpErrorFilter }],
 })
---
+++ src/idea/idea.dto.ts
@@ -0,0 +1,8 @@
+export interface IdeaDTO {
+  tittle: string;
+  description: string;
+  image?: string;
+  author: string;
+  user_id: string;
+  created_at: Date;
+}
